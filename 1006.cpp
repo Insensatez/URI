a370656b1e48734a7b2c06c2001e9722be22cde8
Insensatez <m.salomao.br@gmail.com>
2018-09-24 11:55:55 -0500

    add 1006

---
+++ 1006.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double a, b, c;
+    
+    cin >> a >> b >> c;
+
+    cout.precision(1);
+
+    cout << "MEDIA = " << fixed << ((a * 2) + (b * 3) + (c * 5)) / 10 << endl;
+ 
+     return 0;
+}
